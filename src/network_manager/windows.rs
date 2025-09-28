6fcf4be74f9cd809f2ef1654a3c9cc468cb0e1f0
Chris Hulme <chris_hulme99@outlook.com>
2025-09-28 11:20:39 +0100

    Use coverage(off)

--- src/network_manager/windows.rs
+++ src/network_manager/windows.rs
@@ -78,8 +78,7 @@ pub trait WlanApi {
 
 /// Concrete implementation of the [`WlanApi`] trait using actual Windows API calls.
 pub struct WlanApiImpl;
-
-#[no_coverage]
+#[coverage(off)]
 impl WlanApi for WlanApiImpl {
     // All methods implement the real Windows API logic, as in your current code
     unsafe fn open_handle(
